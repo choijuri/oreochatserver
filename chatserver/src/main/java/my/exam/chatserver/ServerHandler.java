0cc08d192ed7f400901152a5c6827e49837b040e
irerin07 <counterchord89@gmail.com>
2019-01-04 13:45:53 +0900

    let's do it

--- chatserver/src/main/java/my/exam/chatserver/ServerHandler.java
+++ chatserver/src/main/java/my/exam/chatserver/ServerHandler.java
@@ -20,7 +20,7 @@ public class ServerHandler implements Runnable {
         ChatUser chatUser = new ChatUser(socket);
         String nickname = chatUser.read();
         chatUser.setNickname(nickname);
-        System.out.println("message : " + nickname);
+        System.out.println("nick : " + nickname);
 
         chatLobby.addChatUser(chatUser);
 
@@ -28,6 +28,7 @@ public class ServerHandler implements Runnable {
             while (true) {
                 String message = chatUser.read();
                 System.out.println(chatUser.getNickname() + "님의 입력 : " + message);
+                System.out.println(inRoom);
                 if (!inRoom) { // 로비에 있을 경우
                     if (message.indexOf("/create") == 0) {
                         String title = message.substring(message.indexOf(" ") + 1);
@@ -53,45 +54,9 @@ public class ServerHandler implements Runnable {
                     }
                 } else { // 방안에 있을 경우
                     if (message.indexOf("/whisper") == 0) {
-                        String[] s = message.split(" ");
-                        String whisperTo = s[1];
-                        System.out.println("whisper to: " + whisperTo);
-                        String msg = message.substring(message.indexOf(s[2]));
-                        List<ChatUser> chatUsers = chatLobby.getUser(chatUser);
-                        for (ChatUser cu : chatUsers) {
-                            if(cu.getNickname().equals(whisperTo)){
-                                cu.write("[" + chatUser.getNickname()+ "님의 귓속말]" + msg);
-
-                            }
-                        }
-
                     } else if (message.indexOf("/kick") == 0) {
-                        if (chatUser.roomMaster() == true) {
-                            String kickWho = message.substring(message.indexOf(" ") + 1);
-                            List<ChatUser> chatUsers = chatLobby.getUser(chatUser);
-                            for (ChatUser cu : chatUsers) {
-                                if (cu.getNickname().equals(kickWho)) {
-                                    cu.close();
-                                }else if(chatUser.getNickname().equals(kickWho)){
-                                    chatUser.write("자기자신은 강퇴시킬 수 없습니다.");
-                                }else{
-                                    chatUser.write(kickWho + " 해당하는 유저가 없습니다.");
-                                }
-                            }
-                        } else {
-                            chatUser.write("방장이 아닙니다.");
-                        }
                     } else if(message.indexOf("/giveMaster") == 0){
-
-
                     } else if (message.indexOf("/change") == 0) {
-                        String name = message.substring(message.indexOf(" ") + 1);
-                        List<ChatUser> lists = chatLobby.getUser(chatUser);
-                        for (ChatUser cu : lists) {
-                            if (cu.getNickname().equals(chatUser.getNickname())) {
-                                cu.setNickname(name);
-                            }
-                        }
                     }else {
                         List<ChatUser> chatUsers = chatLobby.getUser(chatUser);
                         for (ChatUser cu : chatUsers) {
